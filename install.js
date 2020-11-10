6cace1209025e50f0b01c625a078d5f2e6e0fbe6
fernik <nikejoelmadrid@gmail.com>
2020-11-09 22:23:26 -0500

    instalador de jspatron y sus dependencias

---
+++ install.js
@@ -0,0 +1,19 @@
+const version = require("./version");
+const exec = require("./exec");
+const path = require('path');
+
+version(function (versions) {
+    const lasted = versions[0];
+    const name_dir = process.argv[2] || '';
+    const download = require('download-git-repo')
+
+    download(`direct:https://github.com/everskyblue/patronjs/archive/${lasted.name}.tar.gz`, resolve(name_dir), function (err) {
+        console.log(err ? 'an error occurred in the installation process\n' : 'installed!\n')
+        console.error(error);
+        exec(name_dir);
+    })
+})
+
+function resolve(...dirs) {
+    return path.resolve(process.cwd(), ...dirs);
+}
