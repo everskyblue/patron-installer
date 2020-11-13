970fa447d80b31ca8f1ab67099e90effd391b2c0
fernik <nikejoelmadrid@gmail.com>
2020-11-13 06:45:56 -0500

    proceso de instalacion diferente, sin descargar el tarball

--- install.js
+++ install.js
@@ -1,19 +1,15 @@
 #!/usr/bin/env node
 
-const version = require("./version");
 const exec = require("./exec");
 const path = require('path');
 
-version(function (versions) {
-    const lasted = versions[0];
-    const name_dir = process.argv[2] || '';
-    const download = require('download-git-repo')
+const name_dir = process.argv[2] || '';
+const download = require('download-git-repo')
 
-    download(`direct:https://github.com/everskyblue/patronjs/archive/${lasted.name}.tar.gz`, resolve(name_dir), function (err) {
-        console.log(err ? 'an error occurred in the installation process\n' : 'installed!\n')
-        console.error(err);
-        exec(name_dir);
-    })
+download('everskyblue/patronjs', resolve(name_dir), function (err) {
+    console.log(err ? 'an error occurred in the installation process\n' : 'installed!\n')
+    console.error(err);
+    exec(name_dir);
 })
 
 function resolve(...dirs) {
